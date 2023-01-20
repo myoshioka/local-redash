d5d11260654a47f775ac5b0be16f4a74c7488d8d
Makoto Yoshioka <y.mako2@gmail.com>
2023-01-20 19:11:12 +0900

    リファクタリング

--- local_redash/main.py
+++ local_redash/main.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from os.path import join, dirname
 import click
 from local_redash.containers import Container
-from local_redash.command_executer import CommandExecuter
 from dependency_injector.wiring import Provide, inject
 
 os.environ['NO_PROXY'] = '127.0.0.1,localhost'
@@ -20,11 +19,9 @@ def main(command_name: str,
          container: Container = Provide[Container]):
 
     container.config.command.type.from_value(command_name)
-    print(query_file)
-    print(data_source_id)
 
-    executer = container.executer()
-    executer.execute()
+    command_executer = container.executer()
+    command_executer.execute()
 
 
 if __name__ == '__main__':
@@ -34,7 +31,6 @@ if __name__ == '__main__':
     container = Container()
     container.config.redash.url.from_env("REDASH_URL")
     container.config.redash.api_key.from_env("API_KEY")
-    # container.config.command.type.from_value(command_name)
     container.wire(modules=[__name__])
 
     main()
