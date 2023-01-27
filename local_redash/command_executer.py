1c4a3d4abada1e5258f83a465849f05ed6f0585c
Makoto Yoshioka <y.mako2@gmail.com>
2023-01-27 16:15:28 +0900

    コマンド実行クラスの処理を変更

--- local_redash/command_executer.py
+++ local_redash/command_executer.py
@@ -1,11 +1,16 @@
 from local_redash.commands.base import Command
+from tabulate import tabulate
 
 
 class CommandExecuter:
-
     def __init__(self, command: Command):
         self._command = command
 
-    def execute(self) -> None:
+    def execute(self, *args) -> None:
         print(f'execute command: {self._command.__class__.__name__}')
-        self._command.execute()
+        result = self._command.execute(*args)
+        print(
+            tabulate(result,
+                     headers="keys",
+                     tablefmt="psql",
+                     stralign='center'))
