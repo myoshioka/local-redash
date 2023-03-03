d9156b1001a0a9582ae572275780c51862512e7f
Makoto Yoshioka <y.mako2@gmail.com>
2023-03-03 19:03:27 +0900

    テストコードを修正

--- test/test_command.py
+++ test/test_command.py
@@ -1,15 +1,35 @@
 from unittest import mock
 
 
-def test_data_source_list(test_container, monkeypatch):
+def test_data_source_list(test_container, monkeypatch, capsys):
     test_container.config.command.type.from_value('data_source_list')
 
-    mock_value = [{'aaaa': 'AAAA'}, {'bbbb': 'BBBB'}]
+    expected_format = "\n".join([
+        '+--------+--------+--------+',
+        '|   aaaa |   bbbb |   cccc |',
+        '|--------+--------+--------|',
+        '|   1111 |   2222 |   3333 |',
+        '|   4444 |   5555 |   6666 |',
+        '+--------+--------+--------+',
+    ])
+    mock_value = [{
+        'aaaa': '1111',
+        'bbbb': '2222',
+        'cccc': '3333'
+    }, {
+        'aaaa': '4444',
+        'bbbb': '5555',
+        'cccc': '6666'
+    }]
     redash_client_mock = mock.Mock()
     redash_client_mock.get_data_source_list.return_value = mock_value
 
     with test_container.redash_client.override(redash_client_mock):
-        command = test_container.command()
-        result = command.execute()
+        executer = test_container.executer()
+        executer.execute()
 
-    assert result == mock_value
+    captured = capsys.readouterr()
+    print('\n')
+    print(captured.out)
+    print(expected_format)
+    assert captured.out.splitlines() == expected_format.splitlines()
