965b20bdbc365db7464336eebdd186e6d3eb87ff
Makoto Yoshioka <y.mako2@gmail.com>
2023-03-03 19:03:07 +0900

    フィルターメソッドで表示するカラムを指定

--- local_redash/commands/query_list.py
+++ local_redash/commands/query_list.py
@@ -1,10 +1,20 @@
-from local_redash.commands.base import Command
+from local_redash.commands.base import Command, ResultData
 from local_redash.lib.redash_client import RedashClient
 
 
 class QueryListCommand(Command):
+
     def __init__(self, client: RedashClient) -> None:
         self._redash_client = client
 
-    def execute(self):
-        return self._redash_client.get_query_list()
+    def execute(self) -> ResultData:
+        result = self._redash_client.get_query_list()
+        keys = set([
+            'id',
+            'name',
+            'created_at',
+            'retrieved_at',
+            'data_source_id',
+            'runtime',
+        ])
+        return self.filter_columns(result, keys)
