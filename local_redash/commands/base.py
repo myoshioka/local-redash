7a428bdfe170e5e6844bdcc71b910b53aa63d34c
Makoto Yoshioka <y.mako2@gmail.com>
2023-03-03 19:02:13 +0900

    データ型ｎ定義とフィルターメソッドの追加

--- local_redash/commands/base.py
+++ local_redash/commands/base.py
@@ -1,9 +1,19 @@
 from typing import Union
+from typing import TypeAlias
+
+ResultData: TypeAlias = list[dict[str, Union[str, int]]]
 
 
 class Command:
+
     def __init__(self) -> None:
         pass
 
-    def execute(self, *args) -> list[dict[str, Union[str, int]]]:
+    def execute(self, *args) -> ResultData:
         raise NotImplementedError()
+
+    def filter_columns(self, result_data: ResultData,
+                       columns: set[str]) -> ResultData:
+        return map(
+            lambda x: dict(filter(lambda item: item[0] in columns, x.items())),
+            result_data)
