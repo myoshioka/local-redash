08c19bb2231ffce672d68e532f94939cdf6ae4f4
Makoto Yoshioka <y.mako2@gmail.com>
2023-03-03 18:55:31 +0900

    不要なprintを削除

--- local_redash/command_executer.py
+++ local_redash/command_executer.py
@@ -3,11 +3,11 @@ from tabulate import tabulate
 
 
 class CommandExecuter:
+
     def __init__(self, command: Command):
         self._command = command
 
     def execute(self, *args) -> None:
-        print(f'execute command: {self._command.__class__.__name__}')
         result = self._command.execute(*args)
         print(
             tabulate(result,
