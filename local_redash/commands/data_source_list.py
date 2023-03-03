5b33ae0ca3190ceae56e2ff8076bd306a15e2065
Makoto Yoshioka <y.mako2@gmail.com>
2023-03-03 19:02:40 +0900

    型定義を追加

--- local_redash/commands/data_source_list.py
+++ local_redash/commands/data_source_list.py
@@ -1,10 +1,11 @@
-from local_redash.commands.base import Command
+from local_redash.commands.base import Command, ResultData
 from local_redash.lib.redash_client import RedashClient
 
 
 class DataSourceListCommand(Command):
+
     def __init__(self, client: RedashClient) -> None:
         self._redash_client = client
 
-    def execute(self):
+    def execute(self) -> ResultData:
         return self._redash_client.get_data_source_list()
--- local_redash/commands/query.py
+++ local_redash/commands/query.py
@@ -1,13 +1,14 @@
 import os
-from local_redash.commands.base import Command
+from local_redash.commands.base import Command, ResultData
 from local_redash.lib.redash_client import RedashClient
 
 
 class QueryCommand(Command):
+
     def __init__(self, client: RedashClient) -> None:
         self._redash_client = client
 
-    def execute(self, query_path: str, data_source_id: str):
+    def execute(self, query_path: str, data_source_id: str) -> ResultData:
         query_str = self.__get_query(query_path)
         query_name = self.__get_file_name(query_path)
 
