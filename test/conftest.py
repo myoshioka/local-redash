bb7946e932cc9d3f41efe76b4120808fb8e68fef
Makoto Yoshioka <y.mako2@gmail.com>
2023-04-07 18:18:45 +0900

    テスト用のjsonファイルを読み込むように変更

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,6 @@
+import os
 import pytest
+import json
 from local_redash.containers import Container
 
 
@@ -9,3 +11,22 @@ def test_container():
             'url': 'http://dummy',
             'api_key': 'dummy_key'
         }})
+
+
+@pytest.fixture
+def mock_value_data_source_list():
+    return get_test_data('data_source_list.json')
+
+
+@pytest.fixture
+def mock_value_query_list():
+    return get_test_data('query_list.json')
+
+
+def get_test_data(filename):
+    folder_path = os.path.abspath(os.path.dirname(__file__))
+    folder = os.path.join(folder_path, 'testdata')
+    jsonfile = os.path.join(folder, filename)
+    with open(jsonfile) as file:
+        data = json.load(file)
+    return data
