331b3b982730339e87bf58a2c1c5c5eca938f273
Makoto Yoshioka <y.mako2@gmail.com>
2023-04-07 18:19:09 +0900

    コマンドテストの追加

--- test/test_command.py
+++ test/test_command.py
@@ -1,7 +1,8 @@
 from unittest import mock
 
 
-def test_data_source_list(test_container, monkeypatch, capsys):
+def test_data_source_list(test_container, mock_value_data_source_list,
+                          monkeypatch, capsys):
     test_container.config.command.type.from_value('data_source_list')
 
     expected_format = "\n".join([
@@ -12,25 +13,34 @@ def test_data_source_list(test_container, monkeypatch, capsys):
         '|  1111  |      2222      |   3333   |        1 |    True     |  hoge  |    2 |',
         '+--------+----------------+----------+----------+-------------+--------+------+',
     ])
-    mock_value = [{
-        'name': 'aaaa',
-        'pause_reason': 'bbbb',
-        'syntax': 'cccc',
-        'paused': '1',
-        'view_only': 'False',
-        'type': 'fuga',
-        'id': '1',
-    }, {
-        'name': '1111',
-        'pause_reason': '2222',
-        'syntax': '3333',
-        'paused': '1',
-        'view_only': 'True',
-        'type': 'hoge',
-        'id': '2',
-    }]
     redash_client_mock = mock.Mock()
-    redash_client_mock.get_data_source_list.return_value = mock_value
+    redash_client_mock.get_data_source_list.return_value = mock_value_data_source_list
+
+    with test_container.redash_client.override(redash_client_mock):
+        executer = test_container.executer()
+        executer.execute()
+
+    captured = capsys.readouterr()
+    print('\n')
+    print(captured.out)
+    print(expected_format)
+    assert captured.out.splitlines() == expected_format.splitlines()
+
+
+def test_query_list(test_container, mock_value_query_list, monkeypatch,
+                    capsys):
+    test_container.config.command.type.from_value('query_list')
+
+    expected_format = "\n".join([
+        '+--------------------------+------+------------------+------------+--------------------------+-----------+',
+        '|       retrieved_at       |   id |   data_source_id |    name    |        created_at        |   runtime |',
+        '|--------------------------+------+------------------+------------+--------------------------+-----------|',
+        '| 2023-04-07T06:31:06.983Z |    2 |                1 | query_test | 2022-09-29T08:48:25.160Z | 0.0163462 |',
+        '| 2022-09-16T10:12:21.974Z |    1 |                1 |  test-01   | 2022-09-16T10:12:37.666Z | 0.0268879 |',
+        '+--------------------------+------+------------------+------------+--------------------------+-----------+',
+    ])
+    redash_client_mock = mock.Mock()
+    redash_client_mock.get_query_list.return_value = mock_value_query_list
 
     with test_container.redash_client.override(redash_client_mock):
         executer = test_container.executer()
