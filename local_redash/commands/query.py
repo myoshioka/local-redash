c84e0582fd5cbf8d14a251691d15ea5d1b665a73
Makoto Yoshioka <y.mako2@gmail.com>
2023-04-14 19:39:03 +0900

    クエリーコマンドを修正

--- local_redash/commands/query.py
+++ local_redash/commands/query.py
@@ -17,13 +17,11 @@ class QueryCommand(Command):
         if target_query is None:
             created_query = self._redash_client.create_query(
                 data_source_id, query_name, query_str)
-            result = self._redash_client.get_fresh_query_result(
-                created_query['id'])
+            result = self._redash_client.query_result(created_query['id'])
 
         else:
-            self._redash_client.update_query(target_query['id'], query_str)
-            result = self._redash_client.get_fresh_query_result(
-                target_query['id'])
+            self._redash_client.update_query(target_query.id, query_str)
+            result = self._redash_client.query_result(target_query.id)
 
         return result
 
