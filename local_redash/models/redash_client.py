0a6307888066958279fa98544d01d9a140497d68
Makoto Yoshioka <y.mako2@gmail.com>
2023-05-25 22:08:55 +0900

    モデルクラスの追加

--- local_redash/models/redash_client.py
+++ local_redash/models/redash_client.py
@@ -77,3 +77,53 @@ class DataSourceList(BaseModel):
 
     def __getitem__(self, item):
         return self.__root__[item]
+
+
+class LastModified(BaseModel):
+    auth_type: str
+    is_disabled: bool
+    updated_at: str
+    profile_image_url: str
+    is_invitation_pending: bool
+    groups: list[int]
+    id: int
+    name: str
+    created_at: str
+    disabled_at: str | None
+    is_email_verified: bool
+    active_at: str
+    email: str
+
+
+class Visualization(BaseModel):
+    description: str
+    created_at: str
+    updated_at: str
+    id: int
+    type: str
+    options: dict
+    name: str
+
+
+class QueryUpdate(BaseModel):
+    id: int
+    name: str
+    api_key: str
+    created_at: str
+    latest_query_data_id: int
+    schedule: str | None
+    description: str | None
+    tags: list[str]
+    updated_at: str
+    options: dict
+    is_safe: bool
+    version: int
+    is_favorite: bool
+    query_hash: str
+    is_archived: bool
+    query: str
+    is_draft: bool
+    data_source_id: int
+    user: User
+    last_modified_by: LastModified
+    visualizations: list[Visualization]
