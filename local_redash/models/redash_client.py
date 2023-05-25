89e9148dcebdb0149588727fbd4f0ce40de1a512
Makoto Yoshioka <y.mako2@gmail.com>
2023-05-25 19:36:24 +0900

    データモデルクラスの追加

--- local_redash/models/redash_client.py
+++ local_redash/models/redash_client.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
+    id: int
     active_at: str
     auth_type: str
     created_at: str
     disabled_at: str | None
     email: str
     groups: list[int]
-    id: int
     is_disabled: bool
     is_email_verified: bool
     is_invitation_pending: bool
@@ -42,8 +42,38 @@ class Query(BaseModel):
     user: User
 
 
+class QueryList(BaseModel):
+    __root__: list[Query]
+
+    def __iter__(self):
+        return iter(self.__root__)
+
+    def __getitem__(self, item):
+        return self.__root__[item]
+
+
 class ResponseQuery(BaseModel):
     count: int
     page: int
     page_size: int
     results: list[Query]
+
+
+class DataSource(BaseModel):
+    id: int
+    name: str
+    pause_reason: str
+    syntax: str
+    paused: int
+    view_only: bool
+    type: str
+
+
+class DataSourceList(BaseModel):
+    __root__: list[DataSource]
+
+    def __iter__(self):
+        return iter(self.__root__)
+
+    def __getitem__(self, item):
+        return self.__root__[item]
