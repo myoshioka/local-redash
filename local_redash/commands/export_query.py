9e2560ef28545dca694aa9556f29dd7ae93a66be
Makoto Yoshioka <y.mako2@gmail.com>
2024-01-11 19:14:37 +0900

    exportコマンドでid指定でも実行出来るようにコマンドクラスを修正

--- local_redash/commands/export_query.py
+++ local_redash/commands/export_query.py
@@ -2,6 +2,7 @@ import os
 
 from local_redash.commands.base import Command, ResultData
 from local_redash.lib.redash_client import RedashClient
+from local_redash.models.redash_client import QueryDetail
 
 
 class ExportQueryCommand(Command):
@@ -10,22 +11,18 @@ class ExportQueryCommand(Command):
         self._redash_client = client
 
     def execute(self, query_key: str | int, file_path: str) -> ResultData:
-        if type(query_key) == str:
-            return self.from_query_name(query_key, file_path)
-        elif type(query_key) == int:
-            return self.from_query_id(query_key, file_path)
-        else:
-            pass
-
-    def from_query_id(self, query_id: int, file_path: str) -> ResultData:
-        pass
-
-    def from_query_name(self, query_name: str, file_path: str) -> ResultData:
         if not os.path.isdir(file_path):
             return []
         file_path = file_path.rstrip(os.path.sep)
 
-        target_query = self._redash_client.search_query(query_name)
+        target_query: QueryDetail | None = None
+
+        if type(query_key) == str:
+            target_query = self.from_query_name(query_key)
+        elif type(query_key) == int:
+            target_query = self.from_query_id(query_key)
+        else:
+            pass
 
         if target_query is None:
             return []
@@ -36,12 +33,23 @@ class ExportQueryCommand(Command):
         formatted_query = self.format_query(target_query.query,
                                             data_source.type)
         result = self._save_query(formatted_query,
-                                  f'{file_path}/{query_name}.sql')
+                                  f'{file_path}/{target_query.name}.sql')
 
         if not result:
             return []
 
         return [{'exported-query': formatted_query}]
+
+    def from_query_id(self, query_id: int) -> QueryDetail | None:
+        return self._redash_client.get_query(query_id)
+
+    def from_query_name(self, query_name: str) -> QueryDetail | None:
+        query = self._redash_client.search_query(query_name)
+
+        if query is None:
+            return None
+
+        return self._redash_client.get_query(query.id)
 
     def _save_query(self, query_str: str, file_path: str) -> bool:
         try:
